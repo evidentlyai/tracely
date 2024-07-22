5c576aa1f40e46511e944bf30b20e0230126610d
Vyacheslav Morov <vyacheslav.morov@gmail.com>
2024-07-22 22:19:54 +0200

    Add version and add release workflow.

--- tracely/src/tracely/__init__.py
+++ tracely/src/tracely/__init__.py
@@ -1,7 +1,9 @@
 from ._tracer_provider import init_tracing
 from .decorators import trace_event
+from ._version import __version__
 
 __all__ = [
     "init_tracing",
     "trace_event",
+    "__version__",
 ]
