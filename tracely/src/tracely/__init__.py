ac08fee63f887f495208d6e17ce79a0791a9ef64
Vyacheslav Morov <vyacheslav.morov@gmail.com>
2024-10-08 17:00:52 +0200

    Add get_current_span function.

--- tracely/src/tracely/__init__.py
+++ tracely/src/tracely/__init__.py
@@ -1,11 +1,13 @@
 from ._tracer_provider import init_tracing
 from .decorators import trace_event
 from .context import create_trace_event
+from .proxy import get_current_span
 from ._version import __version__
 
 
 __all__ = [
     "create_trace_event",
+    "get_current_span",
     "init_tracing",
     "trace_event",
     "__version__",
