cb14e2c23f94537a1ae71c6adde2d35ce27ff3fc
Vyacheslav Morov <vyacheslav.morov@gmail.com>
2025-11-20 18:51:44 +0100

    New version 0.2.11

--- examples/openai_manual_tracing.py
+++ examples/openai_manual_tracing.py
@@ -1,3 +1,4 @@
+import asyncio
 from time import sleep
 from typing import Optional
 
@@ -10,6 +11,7 @@ from tracely import trace_event
 from tracely import get_current_span
 
 client = openai.Client()
+async_cl = openai.AsyncClient()
 
 
 @trace_event()
@@ -23,6 +25,22 @@ def call_openai(input: str) -> Optional[str]:
                 "output": response.usage.output_tokens,
             },
         )
+        return response.output[0].content[0].text
+    return None
+
+
+@trace_event()
+async def async_call_openai(input: str) -> Optional[str]:
+    response = await async_cl.responses.create(model="gpt-4.1", input=input)
+    span = get_current_span()
+    if response:
+        if span:
+            span.update_usage(
+                tokens={
+                    "input": response.usage.input_tokens,
+                    "output": response.usage.output_tokens,
+                },
+            )
         return response.output[0].content[0].text
     return None
 
@@ -69,14 +87,23 @@ def call_with_user_id_explicit(input: str):
     return second_answer
 
 
-if __name__ == "__main__":
+async def main():
     init_tracing(
         default_usage_details=UsageDetails(
             cost_per_token={"input": 2.0 / 1_000_000, "cached_input": 2.0 / 1_000_000, "output": 8.0 / 1_000_000}
         )
     )
+    print(await async_call_openai("What is LLM?"))
 
-    print(call_openai("What is LLM?"))
+
+if __name__ == "__main__":
+    # init_tracing(
+    #     default_usage_details=UsageDetails(
+    #         cost_per_token={"input": 2.0 / 1_000_000, "cached_input": 2.0 / 1_000_000, "output": 8.0 / 1_000_000}
+    #     )
+    # )
+    asyncio.run(main())
+    # print(call_openai("What is LLM?"))
     # print(call_openai_with_helper("What is LLM?"))
     # print(call_openai_with_context("What is LLM?"))
     # print(multiple_calls_openai("What is LLM?"))
--- tracely/src/tracely/_version.py
+++ tracely/src/tracely/_version.py
@@ -1,4 +1,4 @@
-version_info = (0, 2, 10)
+version_info = (0, 2, 11)
 __version__ = ".".join(map(str, version_info))
 
 
